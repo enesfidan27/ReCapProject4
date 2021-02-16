cec3e79eecebd9ec229382f6e3b8384ee81230ac
enesfidan27 <enesfidan27@gmail.com>
2021-02-16 20:00:20 +0300

    güncel

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -9,15 +10,21 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
-            /*  foreach (var car in carManager.GetAll())
-              {
-                  Console.WriteLine(car.Description);
-              }*/
-
-            foreach (var car in carManager.GetCarsByBrandId(1))
+            Console.WriteLine("Galerideki Tüm Araçlar:");
+            foreach (var car in carManager.GetAll())
             {
-                Console.WriteLine(car.Description);
+                Console.WriteLine(car.ModelYear+" Model/"+car.Description+":"+car.DailyPrice+"TL");
             }
+            Console.WriteLine("----------------------------");
+
+            Car car1 = new Car() {  BrandId = 1, DailyPrice = 250, Description = "Model3", ColorId = 2, ModelYear = 2020 };
+            carManager.Add(car1);
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.ModelYear + " Model/" + car.Description + ":" + car.DailyPrice + "TL");
+            }
+            
+
         }
     }
 }
